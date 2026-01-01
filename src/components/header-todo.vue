ed1a80160e28943cff8a63d92e7f713c187f19a4
Guilherme-santt <guisant@icloud.com>
2026-01-01 14:01:22 -0300

    styled header

---
+++ src/components/header-todo.vue
@@ -0,0 +1,55 @@
+<template>
+    <header :class="['header-content']">
+        <input :class="['input-content']" type="text" name="" id="" placeholder="Digite a tarefa...">
+        <button :class="['btn-content']">+</button>
+    </header>
+</template>
+
+<script setup>
+  // Lógica do cabeçalho (se houver)
+</script>
+
+<style scoped>
+    .header-content { 
+        border: 1px solid #e6e6e6; 
+        background-color: white; 
+        border-radius: 10px; 
+        padding: 20px 10px; 
+        width: 490px;
+        display: flex;
+        column-gap: 10px;
+        align-items: start;
+    }
+    
+    .input-content { 
+        border: none; 
+        width: 400px; 
+        border: 1px solid #e6e6e6; 
+        background-color: white; 
+        border-radius: 5px; 
+        padding: 10px; 
+    }
+
+    .input-content:hover {
+        box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px; 
+        transition: 0.3s all;
+        cursor: pointer;
+    }
+
+    .input-content:focus {
+        border: 1px solid #8100cc !important;
+    }
+    
+    .btn-content {
+        cursor: pointer;
+        background-color: #8100cc;
+        border: none;
+        color: white;
+        padding: 10px 20px;
+        border-radius: 10px;
+        transition: transform 0.3s ease; 
+    }
+    .btn-content:hover {
+        transform: scale(1.2);
+    }
+</style>    
---
+++ src/components/todo.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="todo-content">
+        <h1>Todo-list</h1>
+        <HeaderTodo />
+        <BodyTodo />
+    </div>
+</template>
+
+<script setup>
+    import HeaderTodo from './header-todo.vue';
+    import BodyTodo from './body-todo.vue';
+</script>
+
+<style scoped>
+    .todo-content {
+        padding: 50px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-color: rgb(151, 83, 240);
+        border-radius: 10px;
+        width: 1000px;
+        height: 500px;
+    }
+</style>
